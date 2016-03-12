bfd241168b61d9d962d132637fcf6e985ab471e1
Chris <chris@codereviewvideos.com>
2016-03-12 13:48:15 +0000

    init

---
+++ src/AppBundle/Service/GitHubApi.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Service;
+
+class GitHubApi
+{
+
+    public function getProfile($username)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', 'https://api.github.com/users/' . $username);
+
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        return [
+            'avatar_url'  => $data['avatar_url'],
+            'name'        => $data['name'],
+            'login'       => $data['login'],
+            'details'     => [
+                'company'   => $data['company'],
+                'location'  => $data['location'],
+                'joined_on' => 'Joined on ' . (new \DateTime($data['created_at']))->format('d m Y'),
+            ],
+            'blog'        => $data['blog'],
+            'social_data' => [
+                "Public Repos" => $data['public_repos'],
+                "Followers"    => $data['followers'],
+                "Following"    => $data['following'],
+            ]
+        ];
+    }
+
+
+    public function getRepos($username)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', 'https://api.github.com/users/' . $username . '/repos');
+
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        return [
+            'repo_count' => count($data),
+            'most_stars' => array_reduce($data, function ($mostStars, $currentRepo) {
+                return $currentRepo['stargazers_count'] > $mostStars ? $currentRepo['stargazers_count'] : $mostStars;
+            }, 0),
+            'repos' => $data
+        ];
+    }
+
+}
